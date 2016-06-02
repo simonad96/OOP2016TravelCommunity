f3a0ec6a35ae6cf1d55601daba0277d40579c8ae
simonad96 <simonad96@gmail.com>
2016-06-03 01:55:52 +0300

    Contact and Phone Models

---
+++ WebApplication1/WebApplication1/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Contact
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+     
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+        public virtual ICollection<Phone> Phones {get; set;}
+    }
+}
